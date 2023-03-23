e72959918de7ca840e41bcae8d5a0f79b82054c2
anshuman35 <49508586+anshuman35@users.noreply.github.com>
2023-03-23 19:57:46 +0530

    Create anshuman23.java

---
+++ src/demo/share/jfc/anshuman23.java
@@ -0,0 +1,24 @@
+import java.util.regex.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class anshuman23 implements ObjectSerializer
+{
+  
+ static String fk = "zqr"; 
+  
+  public static void main(String[] arg)
+  {
+    
+    Pattern<ObjectSerializer> hi = Pattern.compile(" bbsr1 ");
+    Matcher uw = hi.find("  1");
+    System.out.println(uw);
+    
+    
+    
+    
+  }
+  
+  
+  
+}
